d61d05f4e4a0b83c7f5f39d6b16243cb95a467d1
Krunal-97 <krunalparmar418@gmail.com>
2021-02-12 01:56:31 +0530

    css updated

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -9,3 +9,12 @@
     window.addEventListener("load", Start);
 
 })();
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
