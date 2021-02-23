b171c538cca0f077695ee66546d6fb2e82de6835
Krunal-97 <krunalparmar418@gmail.com>
2021-02-24 00:42:12 +0530

    created a route

---
+++ routes/book.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// connect to our Book Model
+let Book = require('../models/book');
+
+/* GET Route for the Book List page - READ Operation */
+router.get('/', (req, res, next) => {
+    Book.find((err, BookList) => {
+        if(err)
+        {
+            return console.error(err);
+        }
+        else
+        {
+            // console.log(BookList);
+            res.render('content/book', {title: 'Book List', BookList: BookList});       
+        }
+    });
+});
+
+module.exports = router;
