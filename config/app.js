ce51b0bb874614e9ee1dcde6c65477e9a0daea6d
Krunal-97 <krunalparmar418@gmail.com>
2021-02-24 00:43:27 +0530

    paths are updated

--- config/app.js
+++ config/app.js
@@ -18,6 +18,7 @@ mongoDB.once('open', ()=>{
 
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
+let booksRouter = require('../routes/book');
 
 let app = express();
 
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, '../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/book-list', booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
