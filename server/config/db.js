1ea05eaf7a3745cae562cbd539ea325e757b97d6
Krunal-97 <krunalparmar418@gmail.com>
2021-03-01 15:54:03 +0530

    connection with heroku

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/business_contacts"
+    //"URI": "mongodb://localhost/business_contacts"
+    "URI": "mongodb+srv://krunal:mujFq79drVM4IeRh@mongodbcluster.anpg1.mongodb.net/business_contacts?retryWrites=true&w=majority"
 }
